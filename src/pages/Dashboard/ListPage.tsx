61b88bf332004373cb9cc469e71906a53473d774
Mayur Dayal <mayur.dayal97@gmail.com>
2025-02-16 23:11:52 +0530

    feat(app): added ui for ListPage and integrated accordion

--- src/pages/Dashboard/ListPage.tsx
+++ src/pages/Dashboard/ListPage.tsx
@@ -1,6 +1,7 @@
 import Header from "../../components/Header/Header";
 import TabMenu from "../../components/TabMenu/TabMenu";
 import { Accordion } from "../../components/ui/Accordion";
+import PlusIcon from "../../assets/icons/icon-plus.svg?react";
 
 type Accordions = {
   title: string;
@@ -35,8 +36,31 @@ export default function ListPage() {
       <Header />
       <TabMenu />
 
-      <main>
+      <main className="my-8 px-4 md:px-6 xl:px-8">
         <div className="container mx-auto">
+          <div className="hidden py-2 xl:grid xl:grid-cols-4 xl:border-t xl:border-t-gray-300">
+            <div className="px-2">
+              <h3 className="font-mulish font-bold text-gray-700 text-sm">
+                Task name
+              </h3>
+            </div>
+            <div className="px-2">
+              <h3 className="font-mulish font-bold text-gray-700 text-sm">
+                Due on
+              </h3>
+            </div>
+            <div className="px-2">
+              <h3 className="font-mulish font-bold text-gray-700 text-sm">
+                Task Status
+              </h3>
+            </div>
+            <div className="px-2">
+              <h3 className="font-mulish font-bold text-gray-700 text-sm">
+                Task Category
+              </h3>
+            </div>
+          </div>
+
           {accordions.map((accordion, index) => (
             <Accordion
               key={`accordion-${index}`}
@@ -45,10 +69,26 @@ export default function ListPage() {
               color={accordion.color}
               isActive={accordion.isActive}
             >
-              <div className="w-full min-h-[156px] flex items-center justify-center">
-                <p className="font-mulish font-medium text-sm text-[#2F2F2F]">
-                  No Tasks in {accordion.title}
-                </p>
+              <div className="w-full h-[156px] flex flex-col">
+                {accordion.title === "Todo" ? (
+                  <div className="border-b border-b-gray-300 px-6 py-1">
+                    <button
+                      type="button"
+                      className="flex cursor-pointer items-center gap-1 rounded-full border border-transparent px-3 py-2 hover:border-[#7B1984]"
+                    >
+                      <PlusIcon className="h-4 w-4 stroke-[#7B1984]" />
+                      <span className="font-mulish text-sm font-bold uppercase">
+                        Add Task
+                      </span>
+                    </button>
+                  </div>
+                ) : null}
+
+                <div className="flex h-full items-center justify-center">
+                  <p className="font-mulish text-sm font-medium text-[#2F2F2F]">
+                    No Tasks in {accordion.title}
+                  </p>
+                </div>
               </div>
             </Accordion>
           ))}
